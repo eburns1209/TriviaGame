f014b02f26427439bc6786582e0f4746149c6fce
eburns <eburns1209@gmail.com>
2017-03-23 15:36:45 -0400

    correct file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,15 @@
 var questions = [{question: "What city in California did Saved By the Bell take place?", 
-			choices: ["Palisades", "Sacramento", "Oakland", "Malibu"],
-			correctAnswer: 0},
-			{question: "What was the first hit by the band Zack Attack?", 
-			choices: ["Love Me Now", "Friends Forever", "Lovesick", "Did We Ever Have a Chance"],
-			correctAnswer: 3},
-			{question: "In 'The Zack Tapes', who is the first person to put a heart necklace on Zack?", 
-			choices: ["Kelly", "Lisa", "Jessie", "Slater"],
-			correctAnswer: 1},
-			{question: "Who is NOT a sibling of Kelly Kapowski?", 
-			choices: ["Billy", "Nikki", "Kyle", "Mikey"],
-			correctAnswer: 3}];
+				choices: ["Palisades", "Sacramento", "Oakland", "Malibu"],
+				correctAnswer: 0},
+				{question: "What was the first hit by the band Zack Attack?", 
+				choices: ["Love Me Now", "Friends Forever", "Lovesick", "Did We Ever Have a Chance"],
+				correctAnswer: 3},
+				{question: "In 'The Zack Tapes', who is the first person to put a heart necklace on Zack?", 
+				choices: ["Kelly", "Lisa", "Jessie", "Slater"],
+				correctAnswer: 1},
+				{question: "Who is NOT a sibling of Kelly Kapowski?", 
+				choices: ["Billy", "Nikki", "Kyle", "Mikey"],
+				correctAnswer: 3}];
 	
 
 $(document).ready(function(){
@@ -24,12 +24,11 @@ function countDown(){
 
 	if(counter <=0){
 		clearInterval(counter);
+		$(".result").html("<h3>Game Over</h3>");
 		return;
 	}
 	document.getElementById("start").innerHTML=counter + " secs";
-};
-
-
+};//end function countDown
 
 	
 	 var currentQuestion = 0;
@@ -41,49 +40,62 @@ function countDown(){
   	 
 
 function start(){
-	$(".question").html(parseInt(currentQuestion) + 1 + ". " + questions[currentQuestion].question);
+	
+		timer = setInterval(countDown, 1000);
 
-	timer = setInterval(countDown, 1000);
-
-	 for (var i = 0; i < questions.length; i++){
-	 	formHtml = $("#form").append(questions);
-	 	// questions.push(form);
-		// $("#form").append(options);
-
-	for (var j = 0; j < questions[i].choices.length; j++){
+	 	for (var i = 0; i < questions.length; i++){
+	 	formHtml += $("#form").append(questions);
+	 	$(".question").html(parseInt(currentQuestion) + 1 + ". " + questions[currentQuestion].question);
+		formHtml += $("#form").append(questions[i].length);
+	 	
+	 	for (var j = 0; j < questions[i].choices.length; j++){
 		$("#form").append("<input type='radio' name='question-" + i + "' value='"
 			+ questions[i].choices[j] + "''>" + questions[i].choices[j]);
-	}
-  }
- //  	 for (var i = 0; i < options.length; i++) {
- //      formHtml += '<div><input type="radio" name="option" value="' + i + '" id="option' + i + '"><label for="option' + i + '">' +
- //      questions[currentQuestion].choices[i] + '</label></div><br/>';
-}
+		}
+ 		
+ 		}
+ 
+}; //end start function
 
- //    $('#form').html(formHtml);
- //    $("#next").prop('checked', true);//need to check
-  	
+//tried to tally answers
 
-// function nextQuestion(){
-	
-	
-// }
+// var answers = {q1: "a", q2: "d", q3: "b", q4: "d"};
+// $("#checkAns").click(function(e){
+// 	$.each(questions, function(questions, answer){
+// 		if($('input:radio[name="' + question + '"]:checked').val()=== answer)
+// 				alert(question + ' : True');
+// 		else
+// 			alert(question + ' : False');
+// 	});
+// 	e.preventDefault();
+// });
+//end of attempt of tallying answers
 
 
-
-//adding up the score
-// function done() {
-// 	//add up the questions
-    
-// }
 //displaying the score by checking the answers
-// function result(){
+
+// function submitResult(){
+	
+// 	var submit = "done";
+// 	if(counter === 1){
+// 		$("#next").append(results);
+// 	}
+// }
+// $.each($("input[name='question-0']:checked"), function(){
+// 		if($(this).val()=== questions[0].correctAnswer){
+// 			correct++;
+// 		}
+// 		else{
+// 			incorrect++;
+// 		}
+// 	});
+
 // 	$(document).find("#result").text("You scored: " + correctAnswers + " out of " + questions.length);
 //     $(document).find("#result").show();
 
 
 // }
-
+//submit result function
 
 
 
@@ -92,16 +104,9 @@ $("#start").click(function(){
 });
 
 // $("#next").click(function(){
-// 	nextQuestion();
-// })
-
-
-// $("#done").click(function(){
-// 	result();
+// 	submitResult();
 // });
 
-
-// };
 
 });
 
